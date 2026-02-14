4fcd836ae9706578e25f5e6ef066d802c834fee6
ScriptoriumMBE <58643873+ScriptoriumMBE@users.noreply.github.com>
2026-02-13 23:37:24 -0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -590,6 +590,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/site/scripts");
   eleventyConfig.addPassthroughCopy("src/site/styles/_theme.*.css");
   eleventyConfig.addPassthroughCopy({ "src/site/logo.*": "/" });
+  eleventyConfig.addPassthroughCopy("src/site/audio");
   eleventyConfig.addPlugin(faviconsPlugin, { outputDir: "dist" });
   eleventyConfig.addPlugin(tocPlugin, {
     ul: true,
@@ -649,5 +650,8 @@ module.exports = function (eleventyConfig) {
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: false,
     passthroughFileCopy: true,
+    eleventyConfig.addShortcode("audio", function(url) {
+  return `<audio controls style="width: 100%;"><source src="${url}" type="audio/mpeg">Your browser does not support audio.</audio>`;
+});
   };
 };
